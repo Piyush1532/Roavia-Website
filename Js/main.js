fc3794f78bd3eb11f21e3e27ce63600b85556529
Piyush1532 <piyushadav503@gmail.com>
2024-09-01 15:59:03 +0530

    added navbar functionality

---
+++ Js/main.js
@@ -0,0 +1,16 @@
+let navMenu=document.getElementById("nav-menu")
+let navToggle=document.getElementById("nav-toggle")
+let navClose=document.getElementById("nav-close")
+
+
+if(navToggle){
+navToggle.addEventListener("click" ,()=>{
+navMenu.classList.add("show-menu")
+})
+}
+
+if (navClose) {
+    navClose.addEventListener("click",()=>{
+navMenu.classList.remove("show-menu")
+    })
+}
